69e3a65b77eca1f207f10efa1705fddec2041ffc
ggm <unikname19@gmail.com>
2023-10-03 14:21:39 +0545

    implement active and inactive tab

--- public/js/active.js
+++ public/js/active.js
@@ -5,8 +5,10 @@ tabLinks.forEach((tabLink) => {
     console.log('Tab link clicked');
     tabLinks.forEach((link) => {
       link.classList.remove('active-tab');
+      link.classList.add('inactive-tab');
     });
 
     tabLink.classList.add('active-tab');
+    tabLink.classList.remove('inactive-tab');
   });
 });
