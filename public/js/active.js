06ca1ec90eb14408a578eba9f6895de35fc86934
ggm <unikname19@gmail.com>
2023-10-05 16:19:49 +0545

    implement smooth scroll for home link

--- public/js/active.js
+++ public/js/active.js
@@ -1,11 +1,47 @@
-const tabLinks = document.querySelectorAll('.active-tab');
+document.addEventListener('DOMContentLoaded', function () {
+  const tabLinks = document.querySelectorAll('.active-tab');
+  let activeSectionId = null;
 
-tabLinks.forEach((tabLink) => {
-  tabLink.addEventListener('click', (event) => {
-    tabLinks.forEach((link) => {
+  tabLinks.forEach((tabLink) => {
+    tabLink.addEventListener('click', (event) => {
+      tabLinks.forEach((link) => {
         link.setAttribute('aria-selected', 'false');
+      });
+
+      tabLink.setAttribute('aria-selected', 'true');
+    });
+  });
+
+  const homeLink = document.querySelector('#home-link');
+
+  homeLink.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
 
-    tabLink.setAttribute('aria-selected', 'true');
+    if (activeSectionId) {
+      const previousSection = document.getElementById(activeSectionId);
+      if (previousSection) {
+        previousSection.removeAttribute('id');
+      }
+    }
+
+    activeSectionId = null;
+
+    history.pushState(null, null, window.location.pathname);
+  });
+
+  window.addEventListener('scroll', function () {
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        activeSectionId = section.id;
+      }
+    });
   });
 });
