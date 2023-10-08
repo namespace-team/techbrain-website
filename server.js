e1227969403abe0ceee3e7e75c9929bc8f94eeb1
ggm <unikname19@gmail.com>
2023-10-08 13:52:25 +0545

    fixed design for mobile screen 320 px width

--- server.js
+++ server.js
@@ -4,16 +4,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  const ext = path.extname(req.url);
-  if (ext === '.css') {
-    res.type('text/css');
-  } else if (ext === '.js') {
-    res.type('text/javascript');
-  }
-  next();
-});
-
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
