bf9744b462e1a35cd3130199c1817c3a7cacc487
parash-namespace <parash.a@namespace.jp>
2023-10-11 13:00:53 +0545

    Fix

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const ejs = require('ejs');
 const fs = require('fs');
 const path = require('path');
 
-const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+const htmlFilePath = path.join(__dirname, 'index.html');
 const indexEjsFilePath = path.join(__dirname, 'views', 'ejs', 'index.ejs');
 
 // Function to compile the index.ejs file
@@ -10,8 +10,10 @@ const compileIndexEjs = () => {
   if (fs.existsSync(indexEjsFilePath)) {
     const ejsContent = fs.readFileSync(indexEjsFilePath, 'utf-8');
     fs.mkdirSync(path.dirname(htmlFilePath), { recursive: true });
+
     const html = ejs.render(ejsContent, { filename: indexEjsFilePath });
     fs.writeFileSync(htmlFilePath, html, 'utf-8');
+
     console.log(`Compiled ${indexEjsFilePath} to ${htmlFilePath}`);
   } else {
     console.error('index.ejs file not found.');
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('ejs/index');
 });
 
 const port = process.env.PORT || 3000;
